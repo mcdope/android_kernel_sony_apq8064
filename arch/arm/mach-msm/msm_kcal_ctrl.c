69b29a79656a3a47d4e532c08cdf58994b00f293
daeiron <thomasvangerven@gmail.com>
2014-05-16 18:05:04 +0200

    Fix gamma control. Yeeh!
    Change-Id: I05540a4c5e19dbc1e1f916bda0b78c477cb24fc6

--- arch/arm/mach-msm/msm_kcal_ctrl.c
+++ arch/arm/mach-msm/msm_kcal_ctrl.c
@@ -184,77 +184,6 @@ static unsigned int lcd_rgb_working_lut[256] = {
 	0x00fcfcfc, 0x00fdfdfd, 0x00fefefe, 0x00ffffff
 };
 
-/* pixel order : sRBG */
-//static unsigned int lcd_srgb_preset_lut[256] = {
-	/* default linear qlut */
-/*
-	0x00000000, 0x00141414, 0x001c1c1c, 0x00212121,
-	0x00262626, 0x002a2a2a, 0x002e2e2e, 0x00313131,
-	0x00343434, 0x00373737, 0x003a3a3a, 0x003d3d3d,
-	0x003f3f3f, 0x00414141, 0x00444444, 0x00464646,
-	0x00484848, 0x004a4a4a, 0x004c4c4c, 0x004e4e4e,
-	0x00505050, 0x00515151, 0x00535353, 0x00555555,
-	0x00575757, 0x00585858, 0x005a5a5a, 0x005b5b5b,
-	0x005d5d5d, 0x005e5e5e, 0x00606060, 0x00616161,
-	0x00636363, 0x00646464, 0x00666666, 0x00676767,
-	0x00686868, 0x006a6a6a, 0x006b6b6b, 0x006c6c6c,
-	0x006d6d6d, 0x006f6f6f, 0x00707070, 0x00717171,
-	0x00727272, 0x00737373, 0x00757575, 0x00767676,
-	0x00777777, 0x00787878, 0x00797979, 0x007a7a7a,
-	0x007b7b7b, 0x007c7c7c, 0x007d7d7d, 0x007e7e7e,
-	0x00808080, 0x00818181, 0x00828282, 0x00838383,
-	0x00848484, 0x00858585, 0x00868686, 0x00878787,
-	0x00888888, 0x00888888, 0x00898989, 0x008a8a8a,
-	0x008b8b8b, 0x008c8c8c, 0x008d8d8d, 0x008e8e8e,
-	0x008f8f8f, 0x00909090, 0x00919191, 0x00929292,
-	0x00939393, 0x00939393, 0x00949494, 0x00959595,
-	0x00969696, 0x00979797, 0x00989898, 0x00999999,
-	0x00999999, 0x009a9a9a, 0x009b9b9b, 0x009c9c9c,
-	0x009d9d9d, 0x009e9e9e, 0x009e9e9e, 0x009f9f9f,
-	0x00a0a0a0, 0x00a1a1a1, 0x00a2a2a2, 0x00a2a2a2,
-	0x00a3a3a3, 0x00a4a4a4, 0x00a5a5a5, 0x00a5a5a5,
-	0x00a6a6a6, 0x00a7a7a7, 0x00a8a8a8, 0x00a8a8a8,
-	0x00a9a9a9, 0x00aaaaaa, 0x00ababab, 0x00ababab,
-	0x00acacac, 0x00adadad, 0x00aeaeae, 0x00aeaeae,
-	0x00afafaf, 0x00b0b0b0, 0x00b0b0b0, 0x00b1b1b1,
-	0x00b2b2b2, 0x00b2b2b2, 0x00b3b3b3, 0x00b4b4b4,
-	0x00b5b5b5, 0x00b5b5b5, 0x00b6b6b6, 0x00b7b7b7,
-	0x00b7b7b7, 0x00b8b8b8, 0x00b9b9b9, 0x00b9b9b9,
-	0x00bababa, 0x00bbbbbb, 0x00bbbbbb, 0x00bcbcbc,
-	0x00bdbdbd, 0x00bdbdbd, 0x00bebebe, 0x00bebebe,
-	0x00bfbfbf, 0x00c0c0c0, 0x00c0c0c0, 0x00c1c1c1,
-	0x00c2c2c2, 0x00c2c2c2, 0x00c3c3c3, 0x00c4c4c4,
-	0x00c4c4c4, 0x00c5c5c5, 0x00c5c5c5, 0x00c6c6c6,
-	0x00c7c7c7, 0x00c7c7c7, 0x00c8c8c8, 0x00c8c8c8,
-	0x00c9c9c9, 0x00cacaca, 0x00cacaca, 0x00cbcbcb,
-	0x00cbcbcb, 0x00cccccc, 0x00cdcdcd, 0x00cdcdcd,
-	0x00cecece, 0x00cecece, 0x00cfcfcf, 0x00d0d0d0,
-	0x00d0d0d0, 0x00d1d1d1, 0x00d1d1d1, 0x00d2d2d2,
-	0x00d2d2d2, 0x00d3d3d3, 0x00d4d4d4, 0x00d4d4d4,
-	0x00d5d5d5, 0x00d5d5d5, 0x00d6d6d6, 0x00d6d6d6,
-	0x00d7d7d7, 0x00d8d8d8, 0x00d8d8d8, 0x00d9d9d9,
-	0x00d9d9d9, 0x00dadada, 0x00dadada, 0x00dbdbdb,
-	0x00dbdbdb, 0x00dcdcdc, 0x00dcdcdc, 0x00dddddd,
-	0x00dedede, 0x00dedede, 0x00dfdfdf, 0x00dfdfdf,
-	0x00e0e0e0, 0x00e0e0e0, 0x00e1e1e1, 0x00e1e1e1,
-	0x00e2e2e2, 0x00e2e2e2, 0x00e3e3e3, 0x00e3e3e3,
-	0x00e4e4e4, 0x00e4e4e4, 0x00e5e5e5, 0x00e5e5e5,
-	0x00e6e6e6, 0x00e6e6e6, 0x00e7e7e7, 0x00e7e7e7,
-	0x00e8e8e8, 0x00e8e8e8, 0x00e9e9e9, 0x00e9e9e9,
-	0x00eaeaea, 0x00eaeaea, 0x00ebebeb, 0x00ebebeb,
-	0x00ececec, 0x00ececec, 0x00ededed, 0x00ededed,
-	0x00eeeeee, 0x00eeeeee, 0x00efefef, 0x00efefef,
-	0x00f0f0f0, 0x00f0f0f0, 0x00f1f1f1, 0x00f1f1f1,
-	0x00f2f2f2, 0x00f2f2f2, 0x00f3f3f3, 0x00f3f3f3,
-	0x00f4f4f4, 0x00f4f4f4, 0x00f5f5f5, 0x00f5f5f5,
-	0x00f6f6f6, 0x00f6f6f6, 0x00f7f7f7, 0x00f7f7f7,
-	0x00f8f8f8, 0x00f8f8f8, 0x00f9f9f9, 0x00f9f9f9,
-	0x00f9f9f9, 0x00fafafa, 0x00fafafa, 0x00fbfbfb,
-	0x00fbfbfb, 0x00fcfcfc, 0x00fcfcfc, 0x00fdfdfd,
-	0x00fdfdfd, 0x00fefefe, 0x00fefefe, 0x00ffffff
-};
-*/
-
 void static resetWorkingLut(void)
 {
 	memcpy((void *)lcd_rgb_working_lut, (void *)lcd_rgb_linear_lut,
@@ -354,7 +283,7 @@ static ssize_t kgamma_reset_show(struct device *dev,
 
 static struct kcal_data kcal_value = {255, 255, 255};
 
-static int update_lcdc_lut(void)
+int update_preset_lcdc_lut(void)
 {
 	struct fb_cmap cmap;
 	int ret = 0;
@@ -374,6 +303,7 @@ static int update_lcdc_lut(void)
 
 	return ret;
 }
+EXPORT_SYMBOL(update_preset_lcdc_lut);
 
 static int kcal_set_values(int kcal_r, int kcal_g, int kcal_b)
 {
@@ -393,7 +323,7 @@ static int kcal_get_values(int *kcal_r, int *kcal_g, int *kcal_b)
 
 static int kcal_refresh_values(void)
 {
-        return update_lcdc_lut();
+        return update_preset_lcdc_lut();
 }
 
 static bool calc_checksum(unsigned int a, unsigned int b,
@@ -559,35 +489,17 @@ static struct early_suspend msm_kcal_early_suspend_struct_driver = {
 
 int __init kcal_ctrl_init(void)
 {
-#if 0
-	struct kcal_platform_data *kcalPtr;
-
-	addr = kallsyms_lookup_name("klcd_pdata");
-	kcalPtr = (struct kcal_platform_data *)addr;
-	kcalPtr->set_values = kcal_set_values;
-	kcalPtr->get_values = kcal_get_values;
-	kcalPtr->refresh_display = kcal_refresh_values;
-
-#endif
-	unsigned int addr;
-
-	addr =  kallsyms_lookup_name("update_preset_lcdc_lut");
-	*(funcPtr *)addr = (funcPtr)update_lcdc_lut;
 
 	platform_add_devices(msm_panel_devices,
 		ARRAY_SIZE(msm_panel_devices));
 
 	register_early_suspend(&msm_kcal_early_suspend_struct_driver);
 
-	//pr_info("generic kcal ctrl initialized\n");
-	//pr_info("generic kcal ctrl version %d.%d\n",
-	//	KCAL_CTRL_MAJOR_VERSION,
-	//	KCAL_CTRL_MINOR_VERSION); 
 	return platform_driver_register(&this_driver);
 }
 
 device_initcall(kcal_ctrl_init);
 
-//MODULE_DESCRIPTION("Generic MSM KCAL driver");
+MODULE_DESCRIPTION("Generic MSM KCAL driver");
 MODULE_LICENSE("GPL and additional rights");
 
