93ae41a2262d29f38ca77b40a2ecff68d15e2875
TobiasBaeumer@gmail.com <TobiasBaeumer@gmail.com>
2014-06-21 13:23:45 +0200

    Revert "Merge "Perf: Support sw events across hotplug""
    This reverts commit 3b4ca35fbd350895c15ecce24683ab91dc457839.
    
    Conflicts:
    	arch/arm/mach-msm/perf_debug.c

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -5309,7 +5309,6 @@ static struct pmu perf_swevent = {
 	.read		= perf_swevent_read,
 
 	.event_idx	= perf_swevent_event_idx,
-	.events_across_hotplug = 1,
 };
 
 #ifdef CONFIG_EVENT_TRACING
@@ -5404,7 +5403,6 @@ static struct pmu perf_tracepoint = {
 	.read		= perf_swevent_read,
 
 	.event_idx	= perf_swevent_event_idx,
-	.events_across_hotplug = 1,
 };
 
 static inline void perf_tp_register(void)
@@ -5632,7 +5630,6 @@ static struct pmu perf_cpu_clock = {
 	.read		= cpu_clock_event_read,
 
 	.event_idx	= perf_swevent_event_idx,
-	.events_across_hotplug = 1,
 };
 
 /*
@@ -5713,7 +5710,6 @@ static struct pmu perf_task_clock = {
 	.read		= task_clock_event_read,
 
 	.event_idx	= perf_swevent_event_idx,
-	.events_across_hotplug = 1,
 };
 
 static void perf_pmu_nop_void(struct pmu *pmu)
